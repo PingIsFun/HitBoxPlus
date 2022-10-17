b5cfc77048b0482da5bace96f5019cc3e3ad418c
PingIsFun <pingisfun@protonmail.com>
2022-10-17 22:49:30 +0200

    Fixed transparency

--- src/main/java/io/github/pingisfun/hitboxplus/util/BoxRenderUtil.java
+++ src/main/java/io/github/pingisfun/hitboxplus/util/BoxRenderUtil.java
@@ -13,16 +13,12 @@ public class BoxRenderUtil {
     public static void drawBox(MatrixStack matrices, VertexConsumer vertices, Entity entity) {
         Box box = entity.getBoundingBox().offset(-entity.getX(), -entity.getY(), -entity.getZ());
 
-
         Color hitbox_color = ColorUtil.getEntityColor(entity);
-        HitboxPlus.LOGGER.info(String.valueOf(hitbox_color));
-        HitboxPlus.LOGGER.info(String.valueOf(entity));
         float red = 256 - hitbox_color.getRed();
         float green = 256 - hitbox_color.getGreen();
         float blue = 256 - hitbox_color.getBlue();
-        float alpha = hitbox_color.getTransparency();
-        HitboxPlus.LOGGER.info(String.valueOf(alpha));
+        float alpha = hitbox_color.getAlpha();
 
-        WorldRenderer.drawBox(matrices, vertices, box, red, green, blue, 1.0f);
+        WorldRenderer.drawBox(matrices, vertices, box, red, green, blue, alpha);
     }
 }
--- src/main/java/io/github/pingisfun/hitboxplus/util/ColorUtil.java
+++ src/main/java/io/github/pingisfun/hitboxplus/util/ColorUtil.java
@@ -1,23 +1,41 @@
 package io.github.pingisfun.hitboxplus.util;
 
+import io.github.pingisfun.hitboxplus.HitboxPlus;
 import io.github.pingisfun.hitboxplus.ModConfig;
 import me.shedaniel.autoconfig.AutoConfig;
+import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.network.OtherClientPlayerEntity;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
 
 import java.awt.*;
 
 public class ColorUtil {
     public static Color getEntityColor(Entity entity) {
         ModConfig config = AutoConfig.getConfigHolder(ModConfig.class).getConfig();
-        if (entity instanceof OtherClientPlayerEntity) {
-            return ColorUtil.decode(config.players.neutral_color);
+
+        if (entity instanceof PlayerEntity) {
+            if (entity instanceof ClientPlayerEntity) {
+                return ColorUtil.decode(config.players.self_color, config.players.self_alpha);
+            }
+
+            else if (entity instanceof OtherClientPlayerEntity) {
+                return ColorUtil.decode(config.players.neutral_color, config.players.neutral_alpha);
+            }
         }
 
         return Color.blue;
     }
 
-    private static Color decode(int hex) {
-        return Color.decode(String.valueOf(hex));
+    private static Color decode(int hex, int transparency) {
+        int alpha = ((100 - (transparency * 10)) * 25) / 10;
+
+        if (alpha == 0) {
+            alpha = 1;
+        }
+
+        Color rgb = Color.decode(String.valueOf(hex));
+        return new Color(rgb.getRed(), rgb.getGreen(), rgb.getBlue(), alpha);
+
     }
 }
